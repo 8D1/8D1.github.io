2e4e9f69ac0665ca57386a9208cd5d3458716ea1
jackson <Jacksons56@icloud.com>
2021-12-14 09:25:59 -0700

    pushing stage 1/2 of the game

--- GameDir/bB/script.js
+++ GameDir/bB/script.js
@@ -1,20 +1,98 @@
-let canvas = document.getElementById("BaseCanvas")
-let ctx = canvas.getContext("2d")
-
-ctx.beginPath()
-ctx.rect(20,40,50,50)
-ctx.fillStyle = "#ff0000"
-ctx.fill();
-ctx.closePath()
+const canvas = document.getElementById("BaseCanvas");
+const ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.rect(240,160,20,0, Math.PI*2, false)
-ctx.fillStyle = "green"
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.rect(160,10,100,40)
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)"
+ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+ctx.fillStyle = "green";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(160, 10, 100, 40);
+ctx.strokeStyle = "rgba(0,0,255,0.5001)";
 ctx.stroke();
-ctx.closePath()
+ctx.closePath();
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#x`";
+    ctx.fill();
+    ctx.closePath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2)
+
+}
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+    drawPaddle()
+}
+setInterval(draw, 10)
+
+ctx.beginPath()
+ctx.arc(50, 50, 10, 0, Math.PI*2)
+ctx.fillStyle = "#0095DD"
+
+let x = canvas.width/2
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2
+let ballRadius = 10;
+let paddleHeight = 10 ;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth) / 2;
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+let rightPressed
+let leftPressed
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+if(rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width){
+        paddleX = canvas.width - paddleWidth;
+    }
+}
+else if(leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0){
+        paddleX = 0;
+    }
+}
